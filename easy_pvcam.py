42610f694d084d54700d6a4b41fc5afbe4eefb47
Colin Brosseau <colin-nadeau.brosseau@umontreal.ca>
2017-09-21 15:40:54 -0400

    (refactoring) work to get perticular camera comfiguration out of code

--- easy_pvcam.py
+++ easy_pvcam.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Class to use a Pixis 256 camera thru Pvcam32.dll
+Class for user frendly use of a Princeton Instruments camera with PVCAM
 
-Uses Princeton class from https://github.com/ColinBrosseau/PythonPrincetonCamera
+Uses Princeton class from https://github.com/ColinBrosseau/pvcam_PrincetonInstruments_python
 
 :Author:
   Colin-N. Brosseau
@@ -10,41 +10,51 @@ Uses Princeton class from https://github.com/ColinBrosseau/PythonPrincetonCamera
 :Organization:
   Laboratoire Richard Leonelli, Universite de Montreal, Quebec
 
-:Version: 2015.12.16
+:Version: 2017.09
 
 """
 from Princeton_wrapper import Princeton
 from master_Header_wrapper import *
 import matplotlib.pyplot as plt
 import numpy as np
+import yaml
 
 
-
-
-class Pixis(Princeton):
+class Easy_pvcam(Princeton):
     def __init__(self, number=0):
-        #   Mecanical Shutter
-        self._initShutter()  # initialise Logic Output to drive the shutter
-        # Delay (second) for setting of a mecanical shutter
-        self.delayShutter = 0.1  # second
-        # This enum is perticular to our setup. You migth have to reverse it.
-        # We connect the shutter to the Logic Output
-        self._PixisShutterMode = {'closed':ShutterOpenMode.never, 'opened':ShutterOpenMode.presequence}
-        # This is the reversed setup
-        #PixisShutterMode = {'closed':ShutterOpenMode.presequence, 'opened':ShutterOpenMode.never}
-        #reverse of the Shutter Mode (for reading)
-        self._reversePixisShutterMode = {v.name:k for k, v in PixisShutterMode.items()}
+        super(Easy_pvcam, self).__init__()
+        chip_name = self.getParameterCurrentValue('CHIP_NAME').decode('UTF-8').replace(' ','')
 
-        super(Pixis, self).__init__()
-        self.setpoint_temperature = -70  #set temperature (celcius)
-        self.speed = 0  #set ADC speed index
+        # import cameras configuration        
+        with open("config.yaml", 'r') as ymlfile:
+            camera_cfg = yaml.load(ymlfile)
+            
+        # Set camera parameters
+        # Temperature (celcius)
+        self.setpoint_temperature = camera_cfg[chip_name]['setpoint_temperature']
+        # ADC speed index
+        self.speed = camera_cfg[chip_name]['speed']
+        # ADC gain
+        self.gain = camera_cfg[chip_name]['gain']  
+        # Exposure time in second
+        self.exposureTime = camera_cfg[chip_name]['exposureTime']  
         
         #By default, camera is in full frame mode, set it to spectroscopy mode
         #set camera to 1D (vertical binning) acquisition
         self.setSpectroscopy()
 
-        self.gain = 1  #set ADC gain
-        self.exposureTime = 1  # exposure time in second
+        # Mecanical Shutter
+        if 'shutter' in cfg[chip_name]:
+            self._initShutter()  # initialise Logic Output to drive the shutter
+            # Delay (second) for setting of a mecanical shutter
+            self.delayShutter = cfg[chip_name]['shutter']['delay']
+            # This enum is perticular to our setup. You migth have to reverse it.
+            # We connect the shutter to the Logic Output
+            self._ShutterMode = {'closed':ShutterOpenMode.never, 'opened':ShutterOpenMode.presequence}
+            # This is the reversed setup
+            #PixisShutterMode = {'closed':ShutterOpenMode.presequence, 'opened':ShutterOpenMode.never}
+            #reverse of the Shutter Mode (for reading)
+            self._reverseShutterMode = {v.name:k for k, v in self._ShutterMode.items()}
 
     def setImage(self):
         self._ROI = []
@@ -105,11 +115,11 @@ class Pixis(Princeton):
         self.logicOutput = LogicOutput.SHUTTER
         
     def _getShutter(self):
-        return self._reversePixisShutterMode[self.shutterOpenMode.name]
+        return self._reverseShutterMode[self.shutterOpenMode.name]
 
     def _setShutter(self, value):
         exposureTime = self.exposureTime  # Save exposure time
-        self.shutterOpenMode = self._PixisShutterMode[value]
+        self.shutterOpenMode = self._ShutterMode[value]
         # The shutter needs to have an exposure to apply.
         self.measure(self.delayShutter)  # the delay also let time to shutter to set  
         self.exposureTime = exposureTime  # Restore exposure time
@@ -117,7 +127,7 @@ class Pixis(Princeton):
     shutter = property(_getShutter, _setShutter)
 
 if __name__ == '__main__':
-     camera = Pixis()
+     camera = Easy_pvcam()
      print("ROI:")
      print(camera.ROI)
      print("Actual temperature (C):")
@@ -134,8 +144,11 @@ if __name__ == '__main__':
      print(camera.exposureTime)
      print("Simple measurement")
      a = camera.measure(2)  # exposure time = 2 seconds
-     plt.plot(a[0][0][0][0])
+     plt.plot(a[0])
      print("Exposure parameters:")
-     print(a[1][0][0])
+     print(a[1])
+     print("Simple measurement with backgound removal")
+     a = camera.measure(2)  # exposure time = 2 seconds
+     plt.plot(a[0])
      camera.close()
      
